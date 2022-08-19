02e2c61d1757a7c9c7b0e1db6be163e69236d29a
Anna Júlia <102365781+annajuliaft@users.noreply.github.com>
2022-08-19 17:42:02 -0300

    Update java.js

--- java.js
+++ java.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', inseresumário)
+document.addEventListener('DOMContentLoaded', f_load)
 
-function inseresumário() {
-    const ol = document.createElement('ol')
-    ol.id = 'sumario'
-    document.body.prepend(ol)
-    const conth2 = document.querySelectorAll('h2')
-    
+function f_load() {
+
+    const listaMae = document.createElement('ol')
+    document.body.prepend(listaMae)
+    const contH2 = document.querySelectorAll('h2')
+
     for (let i = 0; i < contH2.length; i++) {
-    
+
         const elementos = f_criarLista(`#h2${i + 1}`, contH2[i].textContent)
         listaMae.appendChild(elementos)
         mudarH2(contH2[i], i + 1)
-    }    
+
+    }
 }
 
+// Adicionando os h2 na lista (ol)
 function f_criarLista(ref, texto) {
 
     const li = document.createElement('li')
@@ -23,6 +25,7 @@ function f_criarLista(ref, texto) {
     return li
 }
 
+// Criando texto clicável (link)
 function f_criarLink(href, texto) {
 
     const criarLink = document.createElement('a')
@@ -32,6 +35,7 @@ function f_criarLink(href, texto) {
     return criarLink
 }
 
+// Mudando os textos no H2
 function mudarH2(elemento, num) {
     elemento.textContent = `${num}. ${elemento.textContent}`
     elemento.id = `h2${num}`
@@ -39,7 +43,7 @@ function mudarH2(elemento, num) {
     f_inserirDepois(elemento, voltar)
 }
 
-
+// Inserindo na h2 modificado na tela
 function f_inserirDepois(elemento, ref) {
     const nextElement = elemento.nextElementSibling
     elemento.parentElement.insertBefore(ref, nextElement)
