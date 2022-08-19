d1f8d88ec717543ee15d60b6059e70f76679b2a3
Anna Júlia <102365781+annajuliaft@users.noreply.github.com>
2022-08-19 17:32:06 -0300

    Update java.js

--- java.js
+++ java.js
@@ -13,3 +13,34 @@ function inseresumário() {
         mudarH2(contH2[i], i + 1)
     }    
 }
+
+function f_criarLista(ref, texto) {
+
+    const li = document.createElement('li')
+    const criarLink = f_criarLink(ref, texto)
+    li.appendChild(criarLink)
+
+    return li
+}
+
+function f_criarLink(href, texto) {
+
+    const criarLink = document.createElement('a')
+    criarLink.setAttribute('href', href)
+    criarLink.textContent = texto
+
+    return criarLink
+}
+
+function mudarH2(elemento, num) {
+    elemento.textContent = `${num}. ${elemento.textContent}`
+    elemento.id = `h2${num}`
+    const voltar = f_criarLink('#', 'Voltar')
+    f_inserirDepois(elemento, voltar)
+}
+
+
+function f_inserirDepois(elemento, ref) {
+    const nextElement = elemento.nextElementSibling
+    elemento.parentElement.insertBefore(ref, nextElement)
+}
